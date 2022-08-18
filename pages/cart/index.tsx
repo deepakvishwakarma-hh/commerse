69d62cf264d2b76bf2b7b059b12cd712f860fe15
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-18 12:04:49 +0530

    cart :  page create

---
+++ pages/cart/index.tsx
@@ -0,0 +1,15 @@
+type Cart = () => ReactElement
+import { ReactElement } from "react"
+import * as Chakra from "@chakra-ui/react"
+import { HomeLayout as Layout } from "../../src/components/layouts"
+
+const Cart: Cart = () => {
+    return (
+        <Layout>
+            <Chakra.Box>
+
+            </Chakra.Box>
+        </Layout>
+    )
+}
+export default Cart
