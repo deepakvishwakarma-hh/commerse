622a22ce6424fe79b15ab0ce4768c3dee1096827
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-18 12:06:45 +0530

    Products : onclick to product

--- src/components/elements/Products/index.tsx
+++ src/components/elements/Products/index.tsx
@@ -7,7 +7,7 @@ const Products = ({ product, catalog }: any) => {
             <Chakra.Heading as="h1" py={20} textAlign='center' textTransform={'capitalize'}>  {catalog} </Chakra.Heading>
             <Chakra.Grid gridTemplateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']} flexWrap={'wrap'} justifyContent="center" >
                 {product.map((item: any, index: number) => (
-                    <Chakra.Grid
+                    <Chakra.Grid onClick={() => { Router.push(`${Router.asPath}/${item.slug.current}`) }}
                         boxShadow={'sm'} gridTemplateRows={'300px 80px'} gridTemplateColumns={'auto'} key={index} mx={5} mb={10} overflow="hidden" >
                         <Chakra.Image objectFit={'cover'} src={urlFor(item.image[0]) as any} />
                         <Chakra.Text textAlign={'center'} lineHeight="80px" bg='white' fontSize={20} fontWeight={700} textTransform={'capitalize'}>{item.name}</Chakra.Text>
