c130b2f2b2cdaafce68e7806ae7edfe44ac42354
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-19 13:38:30 +0530

    Image Opt : Products

---
+++ src/components/elements/Products/Product.tsx
@@ -0,0 +1,28 @@
+import Router from "next/router"
+import Image from "next/image";
+import * as Chakra from "@chakra-ui/react"
+import { useNextSanityImage } from 'next-sanity-image';
+import { configuredSanityClient } from "../../../lib/client"
+
+const Product = ({ data }: any) => {
+
+    const imageProps: any = useNextSanityImage(
+        configuredSanityClient,
+        data.image[0]
+    );
+
+    const onClickHandler = () => {
+        Router.push(`${Router.asPath}/${data.slug.current}`)
+    }
+
+    return (
+        <Chakra.Grid onClick={onClickHandler}
+            boxShadow={'sm'} gridTemplateRows={'300px 80px'} gridTemplateColumns={'auto'} mx={5} mb={10} overflow="hidden" >
+
+            <Image  {...imageProps} loader={imageProps.loader} layout="intrinsic" width="500px" height="500px" objectFit="scale-down" alt="none" />
+
+            <Chakra.Text textAlign={'center'} lineHeight="80px" bg='white' fontSize={20} fontWeight={700} textTransform={'capitalize'}>{data.name}</Chakra.Text>
+        </Chakra.Grid>
+    )
+}
+export default Product
--- src/components/elements/Products/index.tsx
+++ src/components/elements/Products/index.tsx
@@ -1,17 +1,12 @@
-import Router from "next/router"
 import * as Chakra from "@chakra-ui/react"
-import { urlFor } from "../../../lib/client"
+import Product from "./Product"
 const Products = ({ product, catalog }: any) => {
     return (
         <Chakra.Box px={10} bg="whitesmoke">
             <Chakra.Heading as="h1" py={20} textAlign='center' textTransform={'capitalize'}>  {catalog} </Chakra.Heading>
             <Chakra.Grid gridTemplateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']} flexWrap={'wrap'} justifyContent="center" >
                 {product.map((item: any, index: number) => (
-                    <Chakra.Grid onClick={() => { Router.push(`${Router.asPath}/${item.slug.current}`) }}
-                        boxShadow={'sm'} gridTemplateRows={'300px 80px'} gridTemplateColumns={'auto'} key={index} mx={5} mb={10} overflow="hidden" >
-                        <Chakra.Image objectFit={'cover'} src={urlFor(item.image[0]) as any} />
-                        <Chakra.Text textAlign={'center'} lineHeight="80px" bg='white' fontSize={20} fontWeight={700} textTransform={'capitalize'}>{item.name}</Chakra.Text>
-                    </Chakra.Grid>
+                    <Product key={index} data={item} />
                 ))}
 
             </Chakra.Grid>
