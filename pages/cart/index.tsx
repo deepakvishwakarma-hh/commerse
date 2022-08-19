8b53f5adad9bbf05624c0c0bfde989a3f177c46b
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-19 11:49:42 +0530

    cart : struct, blank-cart-fallback

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,26 +1,32 @@
 type Cart = () => ReactElement
+import Link from "next/link"
 import { ReactElement } from "react"
 import * as Chakra from "@chakra-ui/react"
-import store, { useAppSelector } from "../../src/redux"
-import { HomeLayout as Layout } from "../../src/components/layouts"
-
-import type { productDetails } from "../../src/redux/cart"
-
 import { useAppDispatch } from "../../src/redux"
 import { addProduct } from "../../src/redux/cart"
-
-
+import store, { useAppSelector } from "../../src/redux"
+import type { productDetails } from "../../src/redux/cart"
 import { Product } from "../../src/components/elements/@Cart"
+import { HomeLayout as Layout } from "../../src/components/layouts"
 
 const Cart: Cart = () => {
 
-
-
     const store = useAppSelector(store => store.cart)
-    console.log(store)
 
-
-
+    if (store.length == 0) {
+        return (
+            <Layout>
+                <Chakra.Center flexDirection={'column'} h="300px" bg="purple.100">
+                    <Chakra.Heading> Cart is Empty! </Chakra.Heading>
+                    <Link href={'/'} passHref >
+                        <Chakra.Button my={5} px={5}>
+                            + Add Product
+                        </Chakra.Button>
+                    </Link>
+                </Chakra.Center>
+            </Layout>
+        )
+    }
 
     return (
         <Layout>
@@ -32,6 +38,8 @@ const Cart: Cart = () => {
                             <Product data={item} key={item.name} />
                         )
                     })}
+
+
 
 
                 </Chakra.Flex>
