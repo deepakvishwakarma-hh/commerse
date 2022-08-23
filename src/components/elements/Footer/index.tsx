53d2937c38244b8004c7ce27a1084ede8be9d2f2
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-23 21:00:49 +0530

    update : footer : auth link

--- src/components/elements/Footer/index.tsx
+++ src/components/elements/Footer/index.tsx
@@ -1,7 +1,7 @@
 
 import Child from "./child"
 import * as Chakra from "@chakra-ui/react"
-
+import Link from "next/link"
 const Footer = () => {
     return (
         <Chakra.Flex p={5} flexDir={['column', 'row', 'row', 'row']}>
@@ -15,7 +15,14 @@ const Footer = () => {
                         gmtrkc@gmail.com
                     </Chakra.Link>
                 </Chakra.Box>
-                <Child heading="information" links={['login', 'register']} />
+
+                <Chakra.Box my={2} pl={5}>
+                    <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'uppercase'}>Information</Chakra.Heading>
+                    <Link href="/auth" passHref>
+                        <Chakra.Text _hover={{ textDecoration: 'underline' }} cursor="pointer" fontWeight={500} textTransform="capitalize" color={'gray'} my={1} > Login</Chakra.Text>
+                    </Link>
+                </Chakra.Box>
+
             </Chakra.Flex>
             <Chakra.Flex flex={1} flexDirection={'column'}>
                 <Child heading="customer service" links={['return policy', 'privacy policy']} />
