a9db71e4f878951fd70d848736e9095a3b8755ac
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-23 20:54:00 +0530

    update Index/page : SSR to SSG

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,40 @@ interface props {
   products: any,
   catalog: any[]
 }
+import Head from "next/head"
+import * as Chakra from '@chakra-ui/react'
 import { client } from "../src/lib/client"
-import { Catalogs } from "../src/components/elements"
-import { HomeLayout } from "../src/components/layouts"
-import { GetServerSideProps } from "next"
+import { HomeLayout as Body } from "../src/components/layouts"
+import Catalog from "../src/components/elements/Catalogs/catalog"
 
 const Home = ({ catalog }: props) => {
   return (
-    <HomeLayout >
-      <Catalogs catalog={catalog} />
-    </HomeLayout>
+    <>
+      <Head>
+        <title>RedKart - e-commerse</title>
+        <meta name="description" content="local e-commerse website, best offers great deals" />
+      </Head>
+      <Body>
+        <Chakra.Box px={[0, 10]} bg="whitesmoke" pb={10}>
+          <Chakra.Heading as="h1" py={20} textAlign='center'>  Categories </Chakra.Heading>
+          <Chakra.Flex flexWrap={'wrap'} justifyContent="center">
+            {catalog.map((item: any, index: number) => (
+              <Catalog key={index} data={item} />
+            ))}
+          </Chakra.Flex>
+        </Chakra.Box>
+      </Body>
+
+    </>
   )
 }
 
+export const getStaticProps = async () => {
+  const query = '*[_type == "catalog"]';
+  const catalog = await client.fetch(query);
+  return {
+    props: { catalog }
+  };
+};
 
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
-  const queryCatalog = '*[_type == "catalog"]';
-  const catalog = await client.fetch(queryCatalog);
-  return { props: { catalog } }
-}
 export default Home
