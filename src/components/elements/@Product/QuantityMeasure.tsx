8e7ed125c8d0456d80fcdce2022618bc5ef76ca1
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-24 11:00:26 +0530

    update QuanitiyMeasure : full props make width : 100%

--- src/components/elements/@Product/QuantityMeasure.tsx
+++ src/components/elements/@Product/QuantityMeasure.tsx
@@ -1,14 +1,16 @@
 import * as  Chakra from "@chakra-ui/react"
 import { Dispatch, SetStateAction } from "react"
 interface props {
-    setQuantity: Dispatch<SetStateAction<number>>, quantity: number
+    setQuantity: Dispatch<SetStateAction<number>>, quantity: number, full?: boolean
 }
 
 
-const QuantityMeasure = ({ setQuantity, quantity }: props) => {
+const QuantityMeasure = ({ setQuantity, quantity, full }: props) => {
 
     const onChange = (event: any) => {
-        setQuantity(parseInt(event.target.value))
+        if (parseInt(event.target.value) > 0) {
+            setQuantity(parseInt(event.target.value))
+        }
     }
     const QuantityUp = () => {
         setQuantity((prev) => prev + 1)
@@ -20,7 +22,7 @@ const QuantityMeasure = ({ setQuantity, quantity }: props) => {
     }
 
     return (
-        <Chakra.Flex borderRadius={'0'} mb={5} alignItems={'center'} borderLeft="5px whitesmoke solid" maxW="300px">
+        <Chakra.Flex borderRadius={'0'} alignItems={'center'} maxW={full ? 'initial' : "300px"}>
             <Chakra.Button fontSize={20} bg="none" borderY={"lightgray 2px solid !important"} borderLeft="lightgray 2px solid !important" borderRadius={'0'} onClick={QuantityUp}>+</Chakra.Button>
             <Chakra.Input border="lightgray 2px solid !important " borderRadius={'0'} onChange={onChange} value={quantity} type='number' />
             <Chakra.Button fontSize={20} bg="none" borderY={"lightgray 2px solid !important"} borderRight="lightgray 2px solid !important" borderRadius={'0'} onClick={QuantityDown}>-</Chakra.Button>
