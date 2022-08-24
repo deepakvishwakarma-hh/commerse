c9d7cc63ae9b16f7292c54e8302cb9167da9d26b
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-24 16:26:52 +0530

    update : Layout : code spilliting

--- src/components/layouts/Home.tsx
+++ src/components/layouts/Home.tsx
@@ -1,23 +1,40 @@
-
-import { ReactComponentElement, useRef } from "react";
-import * as Chakra from "@chakra-ui/react";
-import { Navigation, Footer, Menu } from "../elements"
-
 interface props {
     children: ReactComponentElement<any, any>,
 }
 
+import { Suspense } from 'react'
+import dynamic from 'next/dynamic'
+import { Navigation } from "../elements"
+import * as Chakra from "@chakra-ui/react";
+import { ReactComponentElement, useRef } from "react";
+
+const FooterDynamic = dynamic(() => import("../elements/Footer"), {
+    suspense: true,
+})
+const MenuDynamic = dynamic(() => import("../elements/Menu"), {
+    suspense: true,
+})
+
 const HomeLayout = ({ children }: props) => {
+
     const btnRef = useRef<HTMLButtonElement>(null)
     const { isOpen, onOpen, onClose } = Chakra.useDisclosure()
+
     return (
         <>
             <Navigation {...{ onOpen, btnRef }} />
-            <Menu   {...{ isOpen, onClose, btnRef }} />
+
+            <Suspense fallback={'loading...'}>
+                <MenuDynamic {...{ isOpen, onClose, btnRef }} />
+            </Suspense>
+
             <Chakra.Box mt={'70px'}>
                 {children}
             </Chakra.Box>
-            <Footer />
+
+            <Suspense fallback={'loading...'}>
+                <FooterDynamic />
+            </Suspense>
         </>
     )
 }
