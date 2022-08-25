0191afa4620604b2051a0debbadbc2dbc70789ea
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-25 13:30:07 +0530

    update : Navigation : tabIndex : remove

--- src/components/elements/Navigation/index.tsx
+++ src/components/elements/Navigation/index.tsx
@@ -38,7 +38,7 @@ const Header = ({ onOpen, btnRef }: any) => {
 
                     <Chakra.Flex flex={1} alignItems='center' justifyContent='end'>
 
-                        <Chakra.Button name="menu-search" _hover={{ bg: 'whitesmoke' }} display={'flex'} alignItems="center" justifyContent={'center'} tabIndex={1} ml={5} variant={'unstyled'} onClick={Model.onOpen}>
+                        <Chakra.Button name="menu-search" _hover={{ bg: 'whitesmoke' }} display={'flex'} alignItems="center" justifyContent={'center'} ml={5} variant={'unstyled'} onClick={Model.onOpen}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
