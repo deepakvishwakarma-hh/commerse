7e165ac2188152dc77065854ae13d87cf7ab896e
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-25 13:33:59 +0530

    Up : footer textTransform={'capitalize'}

--- src/components/elements/Footer/child.tsx
+++ src/components/elements/Footer/child.tsx
@@ -3,7 +3,7 @@ import * as Chakra from "@chakra-ui/react"
 const Child = ({ heading, links }: { heading: string; links: string[] }) => {
     return (
         <Chakra.Box my={2} pl={5}>
-            <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'uppercase'}>{heading}</Chakra.Heading>
+            <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'capitalize'}>{heading}</Chakra.Heading>
             {links.map((value: string, index: number) => <Chakra.Text fontWeight={500} textTransform="capitalize" color={'gray'} my={1} key={index}> {value}</Chakra.Text>)}
         </Chakra.Box>
     )
--- src/components/elements/Footer/index.tsx
+++ src/components/elements/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <Chakra.Flex p={5} flexDir={['column', 'row', 'row', 'row']}>
             <Chakra.Flex flex={1} flexDirection={'column'}>
                 <Chakra.Box my={2} pl={5}>
-                    <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'uppercase'}>Contact </Chakra.Heading>
+                    <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'capitalize'}>Contact </Chakra.Heading>
                     <Chakra.Link color="gray" href="tel:8461833731">
                         +918461833731
                     </Chakra.Link> <br />
@@ -17,7 +17,7 @@ const Footer = () => {
                 </Chakra.Box>
 
                 <Chakra.Box my={2} pl={5}>
-                    <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'uppercase'}>Information</Chakra.Heading>
+                    <Chakra.Heading fontSize={17} py={2} as="h5" textTransform={'capitalize'}>Information</Chakra.Heading>
                     <Link href="/auth" passHref>
                         <Chakra.Text _hover={{ textDecoration: 'underline' }} cursor="pointer" fontWeight={500} textTransform="capitalize" color={'gray'} my={1} > Login</Chakra.Text>
                     </Link>
