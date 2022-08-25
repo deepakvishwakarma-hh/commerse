3f1662ac79a560a1d07bb104a78d96aee8389394
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-25 13:26:22 +0530

    update :Navigation buttons name

--- src/components/elements/Navigation/index.tsx
+++ src/components/elements/Navigation/index.tsx
@@ -25,7 +25,7 @@ const Header = ({ onOpen, btnRef }: any) => {
 
                     <Chakra.Flex flex={1}>
 
-                        <Chakra.Button _hover={{ bg: 'whitesmoke' }} ref={btnRef} onClick={onOpen} mx={1} variant={'unstyled'} display="flex" justifyContent={'center'}>
+                        <Chakra.Button name="menu-hamburger" _hover={{ bg: 'whitesmoke' }} ref={btnRef} onClick={onOpen} mx={1} variant={'unstyled'} display="flex" justifyContent={'center'}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                             </svg>
@@ -38,13 +38,13 @@ const Header = ({ onOpen, btnRef }: any) => {
 
                     <Chakra.Flex flex={1} alignItems='center' justifyContent='end'>
 
-                        <Chakra.Button _hover={{ bg: 'whitesmoke' }} display={'flex'} alignItems="center" justifyContent={'center'} tabIndex={1} ml={5} variant={'unstyled'} onClick={Model.onOpen}>
+                        <Chakra.Button name="menu-search" _hover={{ bg: 'whitesmoke' }} display={'flex'} alignItems="center" justifyContent={'center'} tabIndex={1} ml={5} variant={'unstyled'} onClick={Model.onOpen}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
                         </Chakra.Button>
 
-                        <Chakra.Button _hover={{ bg: 'whitesmoke' }} display={'flex'} alignItems="center" justifyContent={'center'} ml={5} variant={'unstyled'} onClick={onCartClickHandler}>
+                        <Chakra.Button name="menu-cart" _hover={{ bg: 'whitesmoke' }} display={'flex'} alignItems="center" justifyContent={'center'} ml={5} variant={'unstyled'} onClick={onCartClickHandler}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" viewBox="0 0 16 16">
                                 <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                             </svg>
