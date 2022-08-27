5b78556b7da290b535f808a6e72884c1235e175d
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-27 15:28:13 +0530

    #

--- src/components/elements/@Product/Varients.tsx
+++ src/components/elements/@Product/Varients.tsx
@@ -2,16 +2,20 @@ interface props {
     varients: varients[]
 }
 import Link from "next/link"
+import { useRouter } from "next/router"
 import * as Chakra from "@chakra-ui/react"
 import { urlFor } from "../../../lib/client"
 import { type varients } from "../../../../pages/catalog/[catalogId]/[productId]"
-
 const Varients = (props: props) => {
+    const router = useRouter()
     return (
         <Chakra.Flex>
             {props.varients.map((item, index) => {
                 return (
-                    <Link href={item.slug.current} passHref key={index}>
+                    <Link href={{
+                        pathname: `/catalog/[catalogId]/${item.slug.current}`,
+                        query: { catalogId: router.query.catalogId },
+                    }} replace passHref key={index}>
                         <Chakra.Flex cursor={'pointer'} border="1px whitesmoke solid" mr={1} flexDir={'column'} >
                             <Chakra.Image objectFit={'contain'} w="100px" h="100px" maxW="100px" maxH="100px" src={urlFor(item.image) as any}></Chakra.Image>
                             <Chakra.Text textTransform={'capitalize'} fontWeight={'bold'} textAlign={'center'}>{item.varient}</Chakra.Text>
@@ -19,7 +23,7 @@ const Varients = (props: props) => {
                     </Link>
                 )
             })}
-        </Chakra.Flex>
+        </Chakra.Flex >
     )
 }
 export default Varients
