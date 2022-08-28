1730599d9c8653d85c7dfcdab49ccc63ab48439c
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-08-28 11:10:14 +0530

    image carousel for xs device

--- src/components/elements/@Product/index.tsx
+++ src/components/elements/@Product/index.tsx
@@ -3,4 +3,6 @@ export { default as QuantityMeasure } from './QuantityMeasure'
 export { default as Block } from "./Block"
 export { default as Sizes } from "./Sizes"
 export { default as Varients } from "./Varients"
+export { default as ImageSlider } from "./ImageSlider"
+
 
